b9db2b03c3a640afe0891e607ae18b2531020c3f
AaronGr767 <aarongroarke2@gmail.com>
2021-04-19 18:28:16 +0100

    Modified code to open window and play audio file when prompted

--- java/src/C19373983/CAVisual.java
+++ java/src/C19373983/CAVisual.java
@@ -3,4 +3,29 @@ package C19373983;
 import ie.tudublin.*;
 
 public class CAVisual extends Visual{
+
+public void settings(){
+    size(1000, 500);
+}
+
+    public void setup()
+    {
+        startMinim();
+
+        loadAudio("heatwaves.mp3");
+    }
+
+    public void keyPressed()
+    {
+        if (key == ' ')
+        {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+        }
+    }
+
+    public void draw(){
+        background(0);
+    }
+
 }
