a2855120f3a03d1c45509b91756773a631466844
AaronGr767 <aarongroarke2@gmail.com>
2021-04-25 16:14:43 +0100

    Created new SideBox.java class

---
+++ java/src/C19373983/SideBox.java
@@ -0,0 +1,16 @@
+package C19373983;
+
+import processing.core.*;
+
+public class SideBox {
+    
+    CAVisual cv;
+
+    public SideBox(CAVisual cv){
+        this.cv = cv;
+    }
+	
+	public void render(){
+        
+	}
+}
