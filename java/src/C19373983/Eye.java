0dc4a62e58629a8d075c6695c4d7fbf7dd835b06
AaronGr767 <aarongroarke2@gmail.com>
2021-04-25 15:00:04 +0100

    Altered some visuals in Eye.java.

--- java/src/C19373983/Eye.java
+++ java/src/C19373983/Eye.java
@@ -12,19 +12,20 @@ public class Eye {
 
     public void render(){
         
-        cv.strokeWeight(3);
+        cv.strokeWeight(4);
         cv.noFill(); 
 
         //float c = PApplet.map(cv.getSmoothedAmplitude(), 0, 1, 80, 255); 
         float c1 = PApplet.map(cv.getSmoothedAmplitude(), 0, 1, 120, 255);
 
-        cv.stroke(c1, 255, 200);
-        cv.fill(c1+40, 255, 200);
-        cv.ellipse(512, 256, 300 + (cv.lerpedAverage * 100), 100 + (cv.lerpedAverage * 100));
+        cv.stroke(c1, 255, 255);
+        cv.fill(c1+40, 255, 255);
+        cv.ellipse(cv.width / 2, cv.height / 2, 600 + (cv.lerpedAverage * 100), 200 + (cv.lerpedAverage * 100));
 
+        cv.strokeWeight(3);
         cv.stroke(c1, 255, 255);
         cv.fill(c1-50, 255, 200);
-        cv.ellipse(512, 256, 100 + (cv.lerpedAverage * 100), 100 + (cv.lerpedAverage * 100));
+        cv.ellipse(cv.width / 2, cv.height / 2, 200 + (cv.lerpedAverage * 100), 200 + (cv.lerpedAverage * 100));
  
    
         
@@ -38,7 +39,7 @@ public class Eye {
         cv.strokeWeight(2);                
         float lastX = cv.width / 2, lastY = cv.height / 2;
 
-        for(int i = 0 ; i < 80 ; i ++){
+        for(int i = 0 ; i < 140 ; i ++){
 
             //float c2 = PApplet.map(cv.getSmoothedAmplitude(), 0, 1, 230, 255);
             cv.stroke(0, 300, 255, 100);
