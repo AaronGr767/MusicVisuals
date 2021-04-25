87b1e07adcddc133c08e8c4f190082ed2f70dd14
AaronGr767 <aarongroarke2@gmail.com>
2021-04-25 13:35:41 +0100

    Drew 2 boxes and inner circle in centerBox.java

--- java/src/C19373983/centerBox.java
+++ java/src/C19373983/centerBox.java
@@ -10,8 +10,36 @@ public class centerBox {
         this.cv = cv;
     }
 
+    private float angle = 0;
+
     public void render(){
-             
+
+        cv.lights();
+        cv.strokeWeight(2);
+        float c = PApplet.map(cv.lerpedAverage, 0, 1, 0, 255);
+        cv.stroke(c, 255, 255);
+        cv.noFill();
+        angle += 0.01f;
+        float b1 = 100 + (20 * cv.lerpedAverage * 10);
+        float b2 = 50 + (20 * cv.lerpedAverage * 10);
+        
+        cv.pushMatrix();
+        cv.translate(cv.width / 2, cv.height / 2, 0);
+        cv.rotateY(angle);
+        cv.rotateX(angle);
+        cv.box(b1);
+        cv.popMatrix();
+
+        cv.pushMatrix();
+        cv.translate(cv.width / 2, cv.height / 2, 0);
+        cv.rotateY(angle + 90f);
+        cv.rotateX(angle + 90f);
+        cv.box(b2);
+        cv.popMatrix();
+        
+        cv.stroke(c, 255, 255);
+        cv.fill(c, 255, 255);
+        cv.ellipse(512, 256, 50 + (cv.lerpedAverage * 100), 50 + (cv.lerpedAverage * 100));     
     }
 
 }
